6d05e3a99d20d707fd15e49f7eea9f3999e05ed8
babyJesus12 <babyjesus7695@gmail.com>
2025-06-07 03:41:11 +0530

    Contract complete

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,47 @@
 use borsh::{BorshDeserialize, BorshSerialize};
-use solana_program::{next_account_info, AccountInfo}, entrypoint, entrypoint::{self, ProgramResult}, msg, pubkey::Pubkey;
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+};
 
-#[derive(BorshSerialize, BorshDeserialize)]
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct Counter {
+    pub count: u32,
+}
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
 enum InstructionType {
     Increment(u32),
-    Decrement(u32)
+    Decrement(u32),
 }
 
 entrypoint!(counter_contract);
 
 pub fn counter_contract(
-    program_id: PublicKey,
+    program_id: &Pubkey,
     accounts: &[AccountInfo],
-    instruction_data: [u8]
-) -> ProgramResult{
+    instruction_data: &[u8],
+) -> ProgramResult {
     let acc = next_account_info(&mut accounts.iter())?;
 
-    let instruction_type = InstructionType::try_from_slice(instruction_data);
-    let mut counter_data = Counter::try_from(&acc.data.borrow())?;
+    let instruction_type = InstructionType::try_from_slice(instruction_data)?;
+    let mut counter_data = Counter::try_from_slice(&acc.data.borrow())?;
 
     match instruction_type {
         InstructionType::Increment(value) => {
-            counter_data.count += value; 
-        },
+            counter_data.count += value;
+        }
         InstructionType::Decrement(value) => {
-            counter_data.count -= value; 
+            counter_data.count -= value;
         }
     }
 
-    counter_data.serialize(&mut *acc.data.borrow_mut()[..])?;
+    counter_data.serialize(&mut &mut acc.data.borrow_mut()[..])?;
 
-    msg!("Contract succded");
+    msg!("Contract succeeded");
 
     Ok(())
 }
